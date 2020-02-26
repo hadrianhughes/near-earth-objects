690cf4c083a79836cb89c5b2fb5bd74f56cd092e
Hadrian Hughes <h.hughes@amaze.com>
2020-02-26 22:01:28 +0000

    Add background colour to display column

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import { colors } from '../../styles/settings';
 import { minWidth } from '../../styles/tools';
 
 export const Grid = styled.main`
@@ -15,12 +16,15 @@ export const ControlsColumn = styled.section`
   ${props => props.show ? css`display: block;` : ''}
 
   ${minWidth('large')} {
+    border-right: 5px solid ${colors.lightGrey};
     display: block;
     grid-column: 1 / span 2;
   }
 `;
 
 export const DisplayColumn = styled.section`
+  background-color: #474550;
+
   ${minWidth('large')} {
     grid-column: 3 / span 6;
   }
